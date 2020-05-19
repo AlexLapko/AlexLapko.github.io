6966b02b0edbca889d7c43c0634112e48ba0fb2d
AlexLapko <50400269+AlexLapko@users.noreply.github.com>
2020-05-19 16:36:03 +0300

    Commit dread

---
+++ dread/js/anchor.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $("#navbar-menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    })
+});
+
+$(document).ready(function () {
+    $("#footer-menu").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    })
+});
+
+$(document).ready(function () {
+    $("#main-works").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    })
+});
+
+$(document).ready(function () {
+    $("#menu-nav").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1500);
+    })
+});
